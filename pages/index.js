f2c8ee0796e60c833a1e1bd50adfea5fdb9f5e13
Angellus Paixão <paixao.angellus@gmail.com>
2024-01-25 18:09:50 -0300

    change to use HomeScreen instead HomePage component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import Link from '../src/components/Link'
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-    return (
-        <>
-            <h1>Alura - Sample Page</h1>
-            <Link href="/faq" passHref>Ir pro FAQ</Link>
-        </>
-    )
-}
+export default HomeScreen;
+
+// import Link from '../src/components/Link'
+// import Footer from '../src/components/patterns/Footer'
+
+// export default function HomePage() {
+//     return (
+//         <>
+//             <h1>Alura - Sample Page</h1>
+//             <Link href="/faq" passHref>Ir pro FAQ</Link>
+//             <Footer />
+//         </>
+//     )
+// }
