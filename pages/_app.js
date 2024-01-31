292061f9257fd9af123f9b68d1d72c854df23f19
Angellus Paixão <paixao.angellus@gmail.com>
2024-01-31 12:38:02 -0300

    commented snipet for GoogleAnalytics integration

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,19 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                {/* -- Aplicação de Google Analytics -- */}
+                {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-2QZDPX3BLK"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.datalayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-2QZDPX3BLK');
+                        `
+                    }}
+                /> */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />
