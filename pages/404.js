edbeec2884b2c4bbd117f780c1330123403192fe
Angellus Paixão <paixao.angellus@gmail.com>
2024-01-31 12:17:07 -0300

    add Head Next component

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import Link from '../src/components/Link'
 
 export default function NotFound() {
     return(
         <>
+            <Head>
+                <title>404 - Página não encontrada</title>
+            </Head>
             <h1>Página não encontrada</h1>
             <h3>Desculpe pelo trastorno! Por favor, volte a última página <Link href="/">ou pra Home</Link>.</h3>
         </>
