2c891e4333393afdaac56a690b71d7e92379bf7d
Angellus Paixão <paixao.angellus@gmail.com>
2024-01-31 12:17:54 -0300

    add Head Next component and getStaticProps function

--- pages/faq.js
+++ pages/faq.js
@@ -1,11 +1,41 @@
+import Head from 'next/head'
 import Link from '../src/components/Link'
 
-export default function FAQ() {
+export async function getStaticProps() {
+    const FQA_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FQA_API_URL)
+            .then((respostaDoServidor) => {
+                return respostaDoServidor.json();
+            })
+            .then((respostaProCliente) => {
+                return respostaProCliente;
+            })
+
+    return {
+        props: {
+            faq,
+        },
+    };
+}
+
+export default function FAQ({ faq }) {
     return(
         <>
+            <Head>
+                <title>FAQ - Curso Next.js</title>
+            </Head>
             <h1>FAQ</h1>
-            {/*TO-DO: Há a necessidade de componente próprio?*/}
             <Link href="/">Retorne ao Home</Link>
+            <ul>
+                {faq.map(({question, answer}) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
